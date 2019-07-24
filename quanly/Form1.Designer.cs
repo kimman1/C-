5faf58ddc821d17aa2ffcf0455aec21483871020
unknown <kimman06@gmail.com>
2019-07-24 20:21:44 +0700

    User Manager

---
+++ quanly/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace quanly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.btLoad = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnClearForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Khách Hàng";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Địa chỉ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số điện thoại";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(219, 42);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(164, 30);
+            this.txtUser.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(219, 86);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(164, 30);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(219, 136);
+            this.txtPhone.Multiline = true;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(164, 30);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // btInsert
+            // 
+            this.btInsert.Location = new System.Drawing.Point(440, 42);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(96, 34);
+            this.btInsert.TabIndex = 6;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            this.btInsert.Click += new System.EventHandler(this.btInsert_Click);
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(440, 88);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(96, 34);
+            this.btDelete.TabIndex = 7;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(440, 136);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(96, 34);
+            this.btExit.TabIndex = 8;
+            this.btExit.Text = "Exit";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // btLoad
+            // 
+            this.btLoad.Location = new System.Drawing.Point(561, 42);
+            this.btLoad.Name = "btLoad";
+            this.btLoad.Size = new System.Drawing.Size(96, 34);
+            this.btLoad.TabIndex = 9;
+            this.btLoad.Text = "Load";
+            this.btLoad.UseVisualStyleBackColor = true;
+            this.btLoad.Click += new System.EventHandler(this.btLoad_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(70, 186);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(514, 97);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(450, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(217, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Copyright by Kim Man Developer";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(561, 86);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(96, 34);
+            this.btnEdit.TabIndex = 12;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnClearForm
+            // 
+            this.btnClearForm.Location = new System.Drawing.Point(561, 136);
+            this.btnClearForm.Name = "btnClearForm";
+            this.btnClearForm.Size = new System.Drawing.Size(96, 34);
+            this.btnClearForm.TabIndex = 13;
+            this.btnClearForm.Text = "Clear Form";
+            this.btnClearForm.UseVisualStyleBackColor = true;
+            this.btnClearForm.Click += new System.EventHandler(this.btnClearForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 325);
+            this.Controls.Add(this.btnClearForm);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btLoad);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btInsert);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "User Manager ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.Button btLoad;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnClearForm;
+    }
+}
+
---
+++ quanly/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Data.SqlClient;  
+using MySql.Data.MySqlClient;
+
+namespace quanly
+{
+    
+
+    public partial class Form1 : Form
+    {
+        string connectionString = "Server=sql12.freemysqlhosting.net;Port=3306;Database=sql12289053;Uid=sql12289053;Pwd=RIn2XtkGUX;charset=utf8";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string connectionString = "Server=localhost;Port=3306;Database=repair;Uid=root;Pwd=;";
+            //string connectionString = "Server=sql12.freemysqlhosting.net;Port=3306;Database=sql12289053;Uid=sql12289053;Pwd=RIn2XtkGUX;";
+            //MySqlConnection con = new MySqlConnection(connectionString);
+            //MySqlCommand cmd = con.CreateCommand();
+            //cmd.CommandText = "select * from user";
+            //con.Open();
+
+
+        }
+        public MySqlConnection conntectToDatasbe()
+        {
+           // string connectionString = "Server=sql12.freemysqlhosting.net;Port=3306;Database=sql12289053;Uid=sql12289053;Pwd=RIn2XtkGUX;";
+            // string connectionString = "Server=localhost;Port=3306;Database=repair;Uid=root;Pwd=;";
+            MySqlConnection con = new MySqlConnection(connectionString);
+            return con;
+        }
+        public void loadDatabase()
+        {
+            listView1.Clear();
+            listView1.Columns.Add("ID");
+            listView1.Columns.Add("Tên Khách Hàng");
+            listView1.Columns.Add("Địa Chỉ");
+            listView1.Columns.Add("Số Điện Thoại");
+            // connection to database 
+            MySqlConnection con = conntectToDatasbe();
+            String query = "select * from user";
+            MySqlDataAdapter ad = new MySqlDataAdapter(query, con);
+            con.Open();
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            int i = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                listView1.Items.Add(row["iduser"].ToString());
+                listView1.Items[i].SubItems.Add(row["username"].ToString());
+                listView1.Items[i].SubItems.Add(row["address"].ToString());
+                listView1.Items[i].SubItems.Add(row["phone"].ToString());
+                i++;
+            }
+            con.Close();
+        }
+        private void btLoad_Click(object sender, EventArgs e)
+        {
+            loadDatabase();
+        }
+
+        private void btInsert_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            if (txtUser.Text == "" && txtAddress.Text == "" && txtPhone.Text == "")
+            {
+                MessageBox.Show("Please type all the customer Information ","Insert check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+       
+            }
+            if (txtUser.Text.Length > 0 && txtAddress.Text.Length > 0 && txtPhone.Text.Length < 10)
+            {
+                MessageBox.Show("Please input at least 10 characters from Phone Text box", "Insert check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            MySqlConnection con = conntectToDatasbe();
+            String query = "INSERT INTO user (username,address,phone) values (@username,@address,@phone)";
+            MySqlCommand cmd = new MySqlCommand(query,con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@username", txtUser.Text);
+            cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+            String phoneData = txtPhone.Text.Replace(" ", "");
+            if (phoneData.Length >= 10)
+            {
+                cmd.Parameters.AddWithValue("@phone", phoneData);
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Insert Successfully", "Insert Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    loadDatabase();
+                    con.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Insert Failed. Please check your connection", "Insert Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    con.Close();
+                }
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            String ID = "";
+            MySqlConnection con = conntectToDatasbe();
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].Selected)
+                {
+                      ID = listView1.Items[i].SubItems[0].Text;
+                }
+            }
+            String query = "delete from user where iduser =" + ID;
+            MySqlCommand cmd = new MySqlCommand(query,con);
+            con.Open();
+            i= cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                MessageBox.Show("Delete Successfuflly", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Delete Failed", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            loadDatabase();
+            txtUser.Text = "";
+            txtAddress.Text = "";
+            txtPhone.Text = "";
+            con.Close();
+            
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            DialogResult rs = MessageBox.Show("Do you confirm to close this ?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if(rs == DialogResult.OK)
+            this.Close();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            String ID = "";
+            MySqlConnection con = conntectToDatasbe();
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].Selected)
+                {
+                    ID = listView1.Items[i].SubItems[0].Text;
+                }
+            }
+            String sql = "update user set username = @username, address=@address, phone=@phone where iduser=" + ID;
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@username", txtUser.Text);
+            cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+            String phoneData = txtPhone.Text.Replace(" ", "");
+            cmd.Parameters.AddWithValue("@phone", phoneData);
+            cmd.ExecuteNonQuery();
+            loadDatabase();
+            con.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].Selected)
+                {
+
+                    txtUser.Text = listView1.Items[i].SubItems[1].Text;
+                    txtAddress.Text = listView1.Items[i].SubItems[2].Text;
+                    String phoneData = listView1.Items[i].SubItems[3].Text;         
+                    txtPhone.Text = phoneData.Substring(0, 3) + " " + phoneData.Substring(3, 3) + " " + phoneData.Substring(6);
+                    
+                }
+            }
+        }
+
+        private void btnClearForm_Click(object sender, EventArgs e)
+        {
+            txtUser.Text = "";
+            txtAddress.Text = "";
+            txtPhone.Text = "";
+        }
+    }
+}
